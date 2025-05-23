ee038cf8bfdd49b6cf8c89bd2762fb512d94cd89
rafih268 <rafi.h2682002@gmail.com>
2025-05-23 17:23:19 +0100

    style(navbar): improve hover background and spacing for navbar links

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -22,12 +22,15 @@
       <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
         <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-transparent">
           <li>
-            <router-link to="/" class="block py-2 px-3 md:p-0 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white">
+            <router-link
+              to="/"
+              class="block py-2.5 px-4 text-black rounded-sm hover:bg-black border-0 hover:text-white transition duration-200 ease-in-out"
+            >
               Home
             </router-link>
           </li>
           <li>
-            <router-link to="/about" class="block py-2 px-3 md:p-0 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white">
+            <router-link to="/about" class="block py-2.5 px-4 text-black rounded-sm hover:bg-black border-0 hover:text-white transition duration-200 ease-in-out">
               About
             </router-link>
           </li>
